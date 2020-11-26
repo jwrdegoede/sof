6e2ffe67f8ee041eb3d9eb7fd792f464daee854f
Hans de Goede <hdegoede@redhat.com>
2020-11-26 20:02:48 +0100

    Hack for building just the topology files

--- tools/topology/get_abi.sh
+++ tools/topology/get_abi.sh
@@ -2,13 +2,13 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright(c) 2019 Intel Corporation. All rights reserved.
 
-MAJOR=`grep '#define SOF_ABI_MAJOR ' $1/src/include/kernel/abi.h | grep -E ".[[:digit:]]$" -o`
-MINOR=`grep '#define SOF_ABI_MINOR ' $1/src/include/kernel/abi.h | grep -E ".[[:digit:]]$" -o`
-PATCH=`grep '#define SOF_ABI_PATCH ' $1/src/include/kernel/abi.h | grep -E ".[[:digit:]]$" -o`
+MAJOR=`grep '#define SOF_ABI_MAJOR ' ../../src/include/kernel/abi.h | grep -E ".[[:digit:]]$" -o`
+MINOR=`grep '#define SOF_ABI_MINOR ' ../../src/include/kernel/abi.h | grep -E ".[[:digit:]]$" -o`
+PATCH=`grep '#define SOF_ABI_PATCH ' ../../src/include/kernel/abi.h | grep -E ".[[:digit:]]$" -o`
 MAJOR_SHIFT=`grep '#define SOF_ABI_MAJOR_SHIFT'\
-	$1/src/include/kernel/abi.h | grep -E ".[[:digit:]]$" -o`
+	../../src/include/kernel/abi.h | grep -E ".[[:digit:]]$" -o`
 MINOR_SHIFT=`grep '#define SOF_ABI_MINOR_SHIFT'\
-	$1/src/include/kernel/abi.h | grep -E ".[[:digit:]]$" -o`
+	../../src/include/kernel/abi.h | grep -E ".[[:digit:]]$" -o`
 
 major_val=$(($MAJOR << $MAJOR_SHIFT))
 minor_val=$(($MINOR << $MINOR_SHIFT))
